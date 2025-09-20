753e3a9e21a4ca23302573170defe4ddf3acaad4
jinhaounimapfoxueban-spec <jinhao.unimapfoxueban@gmail.com>
2025-09-21 05:49:48 +0800

    Create admin.js

---
+++ pages/admin.js
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+export default function Admin() {
+  return (
+    <div>
+      <Head>
+        <title>管理后台</title>
+      </Head>
+      <h1>管理后台</h1>
+      <p>请先登录</p>
+    </div>
+  );
+}
