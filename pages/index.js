7f48ddabba75e3eac1346ea51ed765fb47b06f7d
jinhaounimapfoxueban-spec <jinhao.unimapfoxueban@gmail.com>
2025-09-21 05:50:18 +0800

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>AR项目管理系统</title>
+      </Head>
+      <h1>欢迎使用AR项目管理系统</h1>
+      <p>系统正在维护中...</p>
+    </div>
+  );
+}
