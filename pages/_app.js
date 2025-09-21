fc1aacb83f2fa9fd74b4c54e3f7a5b119f9041b3
jinhaounimapfoxueban-spec <jinhao.unimapfoxueban@gmail.com>
2025-09-21 18:16:22 +0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,4 @@
-import { useEffect } from 'react';
-
 function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    // 在客户端初始化上传目录
-    const initUploadDir = async () => {
-      try {
-        await fetch('/api/init-upload-dir');
-      } catch (error) {
-        console.warn('初始化上传目录失败:', error);
-      }
-    };
-    
-    initUploadDir();
-  }, []);
-
   return <Component {...pageProps} />;
 }
 
