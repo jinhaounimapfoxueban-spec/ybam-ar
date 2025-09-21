3811cc49dafc3227be149cede0ef197ffa543a62
jinhaounimapfoxueban-spec <jinhao.unimapfoxueban@gmail.com>
2025-09-21 18:03:41 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  // 增加上传文件大小限制
+  api: {
+    bodyParser: false,
+    responseLimit: '50mb',
+  },
+  // 允许大文件上传
+  experimental: {
+    serverComponentsExternalPackages: ['formidable'],
+  },
 };
 
 module.exports = nextConfig;
