09e1e659f7484cd5db089f7f29c9ca20522348c7
jinhaounimapfoxueban-spec <jinhao.unimapfoxueban@gmail.com>
2025-09-21 18:16:42 +0800

    Update upload.js

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -1,18 +1,8 @@
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
-
 export default async function handler(req, res) {
-  if (req.method !== 'POST') {
-    return res.status(405).json({ error: 'Method not allowed' });
-  }
-
-  // 在Netlify上，文件上传需要使用其他方式处理
-  // 这里返回一个错误，提示使用其他方法
   res.status(501).json({ 
-    error: '文件上传功能在Netlify上需要使用其他实现方式',
-    suggestion: '考虑使用云存储服务如AWS S3、Cloudinary或Vercel Blob'
+    error: '文件上传功能暂不可用',
+    message: '请使用外部图片和视频URL'
   });
 }
+
+// 移除config，使用默认配置
